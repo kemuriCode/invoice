3b5172c649722cf14fb481f205a7ff13bad93d7a
t0shifollow <marcindymek24@gmail.com>
2018-09-09 18:35:05 +0200

    add value in InvoicePosition

--- src/model/InvoicePosition.java
+++ src/model/InvoicePosition.java
@@ -1,4 +1,36 @@
 package model;
 
 public class InvoicePosition {
+
+    public static final double TAX_5 = 0.05;
+    public static final double TAX_8 = 0.08;
+    public static final double TAX_23 = 0.23;
+
+    private Product product;
+    private  int count;
+    private double tax = TAX_23;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
 }
